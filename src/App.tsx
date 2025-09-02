7129aa5b9ed00a397b5980428ff20361eaabff48
chenjie <785801610@qq.com>
2025-09-02 22:55:59 +0800

    feat: 雪碧图使用

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import ArticleSVG from './assets/svg/article.svg'
+
+function App() {
+  return (
+    <div>
+      <svg>
+        <use xlinkHref="#icon-article"></use>
+      </svg>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import 'virtual:svg-icons-register'
+
+createRoot(document.getElementById('root')!).render(<App />)
